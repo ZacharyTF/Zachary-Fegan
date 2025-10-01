477a807aaf655fa00ea52633fba7538e2c7da10d
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2025-10-01 01:01:00 +0000

    started craps game

--- m3t2.cpp
+++ m3t2.cpp
@@ -14,22 +14,29 @@ using namespace std;
 
 int main() {
     cout << "Let's roll some dice!" << endl;
-    int seed;
-    cout << "What's your lucky number?  ";
-    cin >> seed;
+    int seed = time(0);
+    cout << "Your seed is: " << seed << endl;
+    //cout << "What's your lucky number?  ";
+    //cin >> seed;
     // Seed the RNG
     srand(seed);
 
     const int MAX = 6; // numbers from 1 - 6
-    int roll;
-    roll = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
-    cout << "Your roll was: " << roll << endl;
+    int roll1, roll2, total;
+    roll1 = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
+    cout << "Your roll was: " << roll1 << endl;
     
-    roll = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
-    cout << "Your roll was: " << roll << endl;
+    roll2 = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
+    cout << "Your roll was: " << roll2 << endl;
 
-    roll = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
-    cout << "Your roll was: " << roll << endl;
-
+    total = roll1 + roll2;
+    cout << "Your total roll is: " << total << endl;
+    // Let's play craps!
+    // 7 and 11 - win!
+    // 2 and 12 - lose.
+    // anything else -- comes later.
+    if (total == 7) {
+        cout << "Lucky seven! You win!" << endl;
+    }
     return 0;
 }
