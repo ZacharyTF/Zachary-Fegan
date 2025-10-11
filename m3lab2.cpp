cca834f8e037581b21fa7a49296f40a1eb612ab4
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2025-10-11 22:32:02 +0000

    Started m3lab2

---
+++ m3lab2.cpp
@@ -0,0 +1,19 @@
+/*
+CSC 134
+M3Lab2 - Number Letter Grade to letter conversion
+Zachary Fegan
+10/11/2025
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int grade;
+
+    //Prompt user for the numerical grade
+    cout << "Enter your numerical grade (from 0 - 100): ";
+    cin >> grade;
+    return 0;
+}
