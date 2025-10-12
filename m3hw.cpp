9033769a3f6a3965ed72d4179a9fa8b7123a0bd0
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2025-10-12 22:04:10 +0000

    Question 1 finished

--- m3hw.cpp
+++ m3hw.cpp
@@ -6,3 +6,40 @@ Zachary Fegan
 */
 
 #include <iostream>
+#include <ctime>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    // -------------------------------
+    // Question 1: Simple Chat Bot
+    // -------------------------------
+    cout << "Question 1: Chat Bot" << endl;
+    string userResponse;
+    cout << "Hello, I'm a C++ program!" << endl;
+    cout << "Do you like me? Please type yes or no." << endl;
+    cin >> userResponse;
+
+    if (userResponse == "yes") {
+        cout << "That's great! I'm sure we'll get along." << endl;
+    } else if (userResponse == "no") {
+        cout << "Well, maybe you'll learn to like me later." << endl;
+    } else {
+        cout << "If you're not sure… that's OK." << endl;
+    }
+
+    // -------------------------------
+    // Question 2: Receipt Calculator
+    // -------------------------------
+    
+    // -------------------------------
+    // Question 3: Choose Your Adventure
+    // -------------------------------
+
+    // -------------------------------
+    // Question 4: Math Practice
+    // -------------------------------
+
+    return 0;
+}
