67c9ed0f4790b0ae4af0c8d0f5d839555aa8a37d
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2025-10-23 00:18:13 +0000

    finished m4lab1

--- m4lab1.cpp
+++ m4lab1.cpp
@@ -4,3 +4,28 @@ M4Lab1 - loops
 Zachary Fegan
 10/22/2025
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height, width;
+
+    // Ask the user for dimensions
+    cout << "Enter the height of the block: ";
+    cin >> height;
+    cout << "Enter the width of the block: ";
+    cin >> width;
+
+    // Outer loop for each row (height)
+    for (int i = 0; i < height; i++) {
+        // Inner loop for each column (width)
+        for (int j = 0; j < width; j++) {
+            cout << "* ";
+        }
+        // Move to the next line after printing one row
+        cout << endl;
+    }
+
+    return 0;
+}
