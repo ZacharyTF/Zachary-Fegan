e695f29c1a9305e8df8e4542dabeb1a30689718a
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2025-11-04 00:20:29 +0000

    finished m5t1

--- m5t1.cpp
+++ m5t1.cpp
@@ -6,3 +6,34 @@
 */
 #include <iostream>
 using namespace std;
+
+// Function Prototypes
+void say_hello();
+int give_the_answer();
+int double_a_number(int num);
+
+int main() {
+    cout << "Hello from main()" << endl;
+
+    say_hello();  // Call void function
+
+    int my_answer = give_the_answer();  // Call value-returning function
+    cout << "The answer is: " << my_answer << endl;
+
+    int twotimes = double_a_number(7);  // Call function with parameter
+    cout << "Here's another number: " << twotimes << endl;
+
+    return 0;
+}
+// Function Definitions
+void say_hello() {
+    cout << "Hi from say_hello()" << endl;
+}
+
+int give_the_answer() {
+    return 42;
+}
+
+int double_a_number(int num) {
+    return num * 2;
+}
